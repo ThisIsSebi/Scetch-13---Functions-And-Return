0c7ee251761f3d79f1a2819597249baa4bcf55fb
ThisIsSebi <shoff439@gmail.com>
2025-03-27 09:30:50 +0100

    converting celsius To Fahrenheit or km to Meter with different functions

--- sketch.js
+++ sketch.js
@@ -1,30 +1,32 @@
 function setup() {
   var km = milesToKm(26.3);
-  print(km)
+  print(km);
   var km = milesToKm(100);
-  print(km)
+  print(km);
 
   var km2 = 10;
-  var kmToMeter = kilometerToMeter(km2) 
-  print(km2 + ' km entspricht ' + kmToMeter + 'm')
+  var kmToMeter = kilometerToMeter(km2);
+  print(km2 + " km entspricht " + kmToMeter + "m");
 
   var celsius = 23;
   var celsiusToFahrenheit = celsiusToFahrenheitFunction(celsius);
-  print(celsius + ' celsius entspricht ' + celsiusToFahrenheit + ' Fahrenheit.')
+  print(
+    celsius + " celsius entspricht " + celsiusToFahrenheit + " Fahrenheit."
+  );
 }
 
-function milesToKm(miles){
+function milesToKm(miles) {
   var km = miles * 1.6;
   return km;
 }
 
-function kilometerToMeter(km){
+function kilometerToMeter(km) {
   var kmToMeter = km * 1000;
   return kmToMeter;
 }
 
-function celsiusToFahrenheitFunction(celsius){
-  var fahrenheit = (celsius * 9/5) + 32;
+function celsiusToFahrenheitFunction(celsius) {
+  var fahrenheit = (celsius * 9) / 5 + 32;
   return fahrenheit;
 }
 
